0ea1eaa4e703f653d47aac8b8607f9d4ee7ee267
Illia Pavlenko <illiampavlenko@gmail.com>
2023-03-22 22:01:04 +0200

    add contactsSlice.js + add store.js files

---
+++ src/components/Redux/store.js
@@ -0,0 +1,17 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import contactsReducer from './contactsSlice';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, contactsReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+});
+
+export const persistor = persistStore(store);
